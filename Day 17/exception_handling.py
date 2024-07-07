35a168157a306e25a2f60e1d001e7f40fe70f2ed
Sean Ryan <sfryan95@gmail.com>
2024-07-06 21:49:33 -0700

    day 17 complete

---
+++ Day 17/exception_handling.py
@@ -0,0 +1,9 @@
+# Day 17: 30 Days of python programming
+
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+
+*nordic_countries, es, ru = names
+
+print(nordic_countries)
+print(es)
+print(ru)
